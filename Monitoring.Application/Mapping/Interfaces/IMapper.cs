990192c1e55d8fb7f999c28f9d9559fd8491e81a
Ivan Boganov <ivan.boganov@gmail.com>
2018-07-06 22:09:37 +0300

    Добавил интерфейс для универсального маппера.

---
+++ Monitoring.Application/Mapping/Interfaces/IMapper.cs
@@ -0,0 +1,17 @@
+﻿namespace Monitoring.Application.Mapping.Interfaces
+{
+    /// <summary>
+    /// Маппер
+    /// </summary>
+    public interface IMapper
+    {
+        /// <summary>
+        /// Смапить одну сущность в другую
+        /// </summary>
+        /// <typeparam name="TIn">Тип сущности на входе</typeparam>
+        /// <typeparam name="TOut">Тип сущности на выходе</typeparam>
+        /// <param name="source">Сущность на входе</param>
+        /// <returns>Сущность на выходе</returns>
+        TOut Map<TIn, TOut>(TIn source);
+    }
+}
